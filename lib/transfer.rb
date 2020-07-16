24b781e5762b7e6ee3c405e2cbcb94a27adc1cb9
Ryna Truong <rynatruong@outlook.com>
2020-07-16 03:45:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,3 +14,24 @@ class Transfer
     sender.valid? == receiver.valid? ? true : false
   end
   
+  def execute_transaction
+    if valid? && @sender.balance > @amount && @status == "pending"
+      @sender.deposit(@amount * -1)
+      @receiver.deposit(@amount)
+      @status= "complete"
+    else
+      @status= "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+  
+   def reverse_transfer
+    if @status == "complete"
+      @sender.balance = @sender.balance + @amount
+      @receiver.balance = @receiver.balance - @amount
+      @status = "reversed"
+    end
+  end
+    
+    
+end
